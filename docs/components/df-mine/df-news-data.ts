d988a3ba9ec5a5e47eb26249a8c1e754dc393fad
fullstackren <liferzy@163.com>
2025-01-09 14:04:46 +0800

    feat:ai中文

---
+++ docs/components/df-mine/df-news-data.ts
@@ -0,0 +1,12 @@
+import type { MineData } from './types'
+
+// 最新动态
+export const NEWS_DATA: MineData[] = [
+  {
+    title: 'AI 中文',
+    time: "2025/1/1",
+    desc: '探索智能边界，塑造未来无限可能，这是我搭建的<a href="http://ai.fullstack.ren/" :target="_blank" rel="noreferrer">《Ai 中文》</a>',
+    image: '/news/ai.jpeg',
+    icon: '/icons/jianghu.jpg',
+  }
+]
